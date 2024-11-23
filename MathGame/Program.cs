2cd4f51d37f1f38be4c5fc6feead2d5225f41cb3
chriswebdv <mypc@CLAGOS>
2024-11-23 13:34:48 -0600

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Math Game that uses the 4 basic math operators
+            // Divisions must be int only, from 0 - 100 
+            // Create menu to choose operation
+            // Record previous games inside a list to access them
+
+        }
+
+    }   
+        
+}
