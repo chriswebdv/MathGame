02156ba36e46ea06b18b50761307bc34e06c0e0d
chriswebdv <mypc@CLAGOS>
2024-11-23 14:00:37 -0600

    Move user input inside while statement, complete part of the conditional

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -6,16 +6,30 @@ namespace MathGame
     {
         static void Main(string[] args)
         {
-            // Create menu to choose operation
+            // Create menu to choose math operator
             Console.WriteLine("Welcome to the Math Game!");
-            Console.WriteLine("Please choose operator, add, subtract, multiple or divide");
-            string choice = Console.ReadLine().ToLower();
-            Console.WriteLine(choice);
-
-
-
+            
             // Create Math Game that uses the 4 basic math operators
             // Divisions must be int only, from 0 - 100 
+            while (true)
+            {
+                Console.WriteLine("Please choose operator, add, subtract, multiple or divide");
+                string choice = Console.ReadLine().ToLower();
+                Console.WriteLine("First number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Second number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                if (choice == "")
+                {
+                    break;
+                }
+                if (choice == "add")
+                {
+                    Console.WriteLine($"Your result is {num1 + num2}");
+                }
+            }
+            Console.WriteLine("Got out of the loop");
+
             // Record previous games inside a list to access them
 
         }
