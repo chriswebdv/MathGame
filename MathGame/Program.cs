dd4f0402bc86ef555a20905e2732b323d59181dc
chriswebdv <mypc@CLAGOS>
2024-11-23 13:45:24 -0600

    Write intro of game and players choise using WriteLine and ReadLine methods

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -6,9 +6,16 @@ namespace MathGame
     {
         static void Main(string[] args)
         {
+            // Create menu to choose operation
+            Console.WriteLine("Welcome to the Math Game!");
+            Console.WriteLine("Please choose operator, add, subtract, multiple or divide");
+            string choice = Console.ReadLine().ToLower();
+            Console.WriteLine(choice);
+
+
+
             // Create Math Game that uses the 4 basic math operators
             // Divisions must be int only, from 0 - 100 
-            // Create menu to choose operation
             // Record previous games inside a list to access them
 
         }
